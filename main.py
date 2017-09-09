82bff30d85508df3c2a004985945e9f5ef022be3
노준형 <njsnacker@gmail.com>
2017-09-09 20:25:26 +0900

    현재 수강중인 강좌목록 파싱.  저장방식은 yaml 로 저장함.  지금은 쓰기가 안됨 ㅎㅎ

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import rsa
+import copy
 import requests
+import yaml
+import io
+import os.path
 from bs4 import BeautifulSoup
 
+config_path = "C:\\eclass\\"
+user_config = "user_info.yaml" #사용자 id,pw 수강강좌 목록
+lecture_config = "leture_info.yaml"
 session = requests.Session()
 
+
+##단일사용자를 위한 시스템임
+class Lecture:
+    """
+    """
+
+#설정 경로로부터 파일을 읽어서 사용자 정보 초기화
+def user_init():
+    if os.path.exists(config_path+user_config):
+        #파일이 존재할 경우
+        print("File exist!")
+        print("사용자 정보를 읽어옵니다.")
+        stream = io.FileIO(config_path+user_config,'r')
+        user_info =  yaml.load(stream)
+    else:
+        #파일이 없을경우 = 처음 접속할경우, id, pw 를 입력받아 씀
+        print("사용자 정보가 없습니다. ID 와 PW 를 입력해주세요")
+        user_info = {}
+        user_info['id'] = input("ID : ")
+        user_info['pw'] = input("PW : ")
+        stream = io.FileIO(config_path + user_config, 'w')
+        yaml.dump(user_info, stream, default_flow_style=False)
+    return user_info
+
 def user_login(p_id,p_pw,session):
-    id = p_id.encode()
+    id = str(p_id).encode()
     pw = p_pw.encode()
 
     url_user = "https://eclass.dongguk.edu/User.do"
@@ -26,11 +57,33 @@ def user_login(p_id,p_pw,session):
               'cmd': 'loginUser',
               'userDTO.outsiderYn': 'N'}
     session.post(url_user, data=params)
-    #response = session.post(url_user, data=params)
-    #response = session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
     return session
 
-user_login("st_id","st_pw!!",session)
+#user_info.yaml 과 비교해서 강좌 목록 변경을 확인함
+def lecture_init(user_info, session):
+    response = session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result = soup.find_all("option")
+    lecture_num = result.__len__()
+    tmp_info = copy.deepcopy(user_info)
+    tmp_info['lecture'] = {}
+    for i in range(1,lecture_num):
+        lecture_code, pf_name, null = result[i].attrs['value'].split(',')
+        tmp_info['lecture'].update({lecture_code:pf_name})
+    if (user_info['lecture'].__len__() < tmp_info['lecture'].__len__()):
+        print("수강목록이 변경(추가)이 감지 되었습니다.")
+        #강좌가 새로 추가되었을때
+    elif (user_info['lecture'].__len__() > tmp_info['lecture'].__len__()):
+        print("수강목록이 변경(감소)이 감지 되었습니다.")
+        #강좌가 삭제되었을때
+    else:
+        print("수강목록이 변경이 감지 되었습니다.")
+    print("현재 사용자 정보")
+    print(user_info)
+        #개수는 똑같으나 개수가 변경되었을 경우
 
-response = session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
-print(response.text)
+
+
+user_info = user_init()
+user_login(user_info['id'],user_info['pw'],session)
+lecture_init(user_info, session)
