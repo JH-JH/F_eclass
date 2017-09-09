a733b5dd22fa650ad12f886e480a4024dad830f9
노준형 <njsnacker@gmail.com>
2017-09-09 13:47:30 +0900

    사용자 로그인 함수화 및 세션객체로 변경

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import rsa
 import requests
+from bs4 import BeautifulSoup
 
-#학생 id 와 pw. 추후 암호화 필요
-id = b"st_id".encode('utf8')
-pw = b"st_pw".encode('utf8')
-
-#로그인 url
-url_user = "https://eclass.dongguk.edu/User.do"
-params = {'cmd':'getRsaPublicKey'}
 session = requests.Session()
 
-response = session.get(url_user,params=params)
-result = response.json()
-print(result)
-#n,e 설정 후 Public Key 초기화
-n = int(result['pubKey1'],16) #pubKey1, 256 length(hex)
-e = int(result['pubKey2'],16) #pubKey2, 10001(hex) 65537(int)
-pubKey = rsa.PublicKey(n,e) #Public Key setting
+def user_login(p_id,p_pw,session):
+    id = p_id.encode()
+    pw = p_pw.encode()
 
-idCrypto = rsa.encrypt(id,pubKey)
-pwCrypto = rsa.encrypt(pw,pubKey)
+    url_user = "https://eclass.dongguk.edu/User.do"
+    params = {'cmd': 'getRsaPublicKey'}
+    response = session.get(url_user, params=params)
+    result = response.json()
 
-#hex string 으로 변환
-print(idCrypto.hex())
-print(pwCrypto.hex())
-response = session.post(url,data = {'paramUserId':idCrypto.hex(), 'paramPassword':pwCrypto.hex()})
+    # n,e 설정 후 Public Key 초기화
+    n = int(result['pubKey1'], 16)  # pubKey1, 256 length(hex)
+    e = int(result['pubKey2'], 16)  # pubKey2, 10001(hex) 65537(int)
+    pubKey = rsa.PublicKey(n, e)  # Public Key setting
+
+    idCrypto = rsa.encrypt(id, pubKey)
+    pwCrypto = rsa.encrypt(pw, pubKey)
+
+    params = {'userDTO.paramUserId': idCrypto.hex(),
+              'userDTO.paramPassword': pwCrypto.hex(),
+              'cmd': 'loginUser',
+              'userDTO.outsiderYn': 'N'}
+    session.post(url_user, data=params)
+    #response = session.post(url_user, data=params)
+    #response = session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
+    return session
+
+user_login("st_id","st_pw!!",session)
+
 response = session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
+print(response.text)
