6617390d4a4f967bea172ee24dcd8c4cda80cbd6
JH <jh0116j@gmail.com>
2017-09-10 19:36:33 +0900

    .ui 파일 삭제

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-import sys
-from PyQt5 import QtWidgets, QtGui, uic, QtCore
-from PyQt5.QtCore import pyqtSlot
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(460, 248)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(110, 90, 56, 20))
+        self.label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(110, 120, 56, 20))
+        self.label_2.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_2.setObjectName("label_2")
+        self.userID = QtWidgets.QLineEdit(Dialog)
+        self.userID.setGeometry(QtCore.QRect(170, 90, 113, 20))
+        self.userID.setObjectName("userID")
+        self.userPW = QtWidgets.QLineEdit(Dialog)
+        self.userPW.setGeometry(QtCore.QRect(170, 120, 113, 20))
+        self.userPW.setObjectName("userPW")
+        self.LoginButton = QtWidgets.QPushButton(Dialog)
+        self.LoginButton.setGeometry(QtCore.QRect(290, 120, 75, 23))
+        self.LoginButton.setObjectName("LoginButton")
+
+        self.retranslateUi(Dialog)
+        self.LoginButton.clicked['bool'].connect(Dialog.LoginClicked)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "ID:"))
+        self.label_2.setText(_translate("Dialog", "PW:"))
+        self.LoginButton.setText(_translate("Dialog", "Login"))
+
 
 class Login(QtWidgets.QDialog):
     def __init__(self, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
-        self.ui = uic.loadUi("Loginform.ui", self)
-        self.ui.show()
 
-    @pyqtSlot()
     def LoginClicked(self):
-        global  app
-        if self.ui.userID.text() != "" and self.ui.userPW.text() != "":
-            print(self.ui.userID.text(), self.ui.userPW.text())
-            sys.exit(app.exec())
+        global app
+        #if self.ui.userID.text() != "" and self.ui.userPW.text() != "":
+        print(self.ui.userID.text(), self.ui.userPW.text())
+        app.exit()
 
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    import sys
     app = QtWidgets.QApplication(sys.argv)
-    w = Login()
-    sys.exit(app.exec())
+    Dialog = Login()
+    Dialog.ui = Ui_Dialog()
+    Dialog.ui.setupUi(Dialog)
+    Dialog.show()
+    app.exec_()
+
+
+    app.exec_()
+
