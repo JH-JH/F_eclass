b46ec6b0ecf28abe2708c492be67e1e1ea8d3d71
노준형 <njsnacker@gmail.com>
2017-12-30 03:50:54 +0900

    리팩토링, 가상환경(venv)추가

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# F_eclass
-이클래스UI가 마음에 안든다
----
-# Build info
+# F_eclass  
+이클래스 UX가 마음에 안들어서 만들어보는 동국대 Eclass 헬퍼  
+***
+# Build info (venv)
 - python version **3.5.0**
 - rsa **3.4.2** (https://stuvel.eu/python-rsa-doc/index.html)
 - requests **2.18.4** (http://docs.python-requests.org/en/master/user/advanced/)
 - beautifulsoup4 **4.6.0** (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
-- PyYAML **3.12** (http://pyyaml.org/)
-- PyQt5 **5.6.0** (https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.6/)
+- PyQt5 **5.6.0** (https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.6/)  
 
----
+_\* 동봉된 virtual env를 사용하는것을 권장합니다._  
+***
 # Contact
 - njsnacker@gmail.com
 - jh0116j@gmail.com
---
+++ system.py
@@ -0,0 +1,187 @@
+import os, sqlite3
+from bs4 import BeautifulSoup
+import requests, rsa, re
+
+programName = "donggukEclassHelper"
+
+class User():
+    __Instance = None
+    __studentNumber = None
+    __name = None
+    __session = None
+
+    def __init__(self):
+        if self.__Instance is not None:
+            raise ValueError("instance already exist!")
+        else:  # constructor
+            self.__session = requests.Session()
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__Instance is None:
+            cls.__Instance = User()
+        return cls.__Instance
+
+    def login(self, studentNumber, password):
+        id = str(studentNumber).encode()
+        pw = str(password).encode()
+        url = "https://eclass.dongguk.edu/User.do"
+        params = {'cmd': 'getRsaPublicKey'}
+        response = self.__session.get(url, params=params)
+        result = response.json()
+
+        # n,e 설정 후 Public Key 초기화
+        n = int(result['pubKey1'], 16)  # pubKey1, 256 length(hex)
+        e = int(result['pubKey2'], 16)  # pubKey2, 10001(hex) 65537(int)
+        pubKey = rsa.PublicKey(n, e)  # Public Key setting
+        idCrypto = rsa.encrypt(id, pubKey)
+        pwCrypto = rsa.encrypt(pw, pubKey)
+        params = {'userDTO.paramUserId': idCrypto.hex(),
+                  'userDTO.paramPassword': pwCrypto.hex(),
+                  'cmd': 'loginUser',
+                  'userDTO.outsiderYn': 'N'}
+        self.__session.post(url, data=params)
+        response = self.__session.get("https://eclass.dongguk.edu/Main.do?cmd=viewHome")
+
+        # 로그인 성공 여부 체크
+        if response.text.find(str(studentNumber)) == -1:
+            return False
+        else:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            exp = r"(?<="+str(studentNumber)+"\().+(?=\))"
+            m = re.search(exp,response.text)
+            self.__name = m.group()
+            return True
+
+class Data():
+    __Instance = None
+    __dataPath = os.environ['LOCALAPPDATA'] + "\\" + programName
+    __documentPath = os.environ['LOCALAPPDATA'] + "\\" + programName
+    __dbConnection = None
+    __cursor = None
+
+    def __init__(self):
+        if self.__Instance is not None:
+            raise ValueError("instance already exist!")
+        else:
+            self.__init()
+
+    def __del__(self):
+        self.__dbConnection.close()
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__Instance is None:
+            cls.__Instance = User()
+        return cls.__Instance
+
+    def __firstCheck(self):
+        isFirst = os.path.exists(self.__dataPath)
+        if isFirst is False:
+
+            return True
+        else:
+            return False
+
+    def __fistInit(self):
+        # 프로그램을 처음 설치 후 실행했을때. 디렉토리와 데이터베이스&테이블을 생성.
+        os.mkdir(self.__dataPath)
+        os.mkdir(self.__documentPath)
+
+        # 데이터베이스 생성 및 테이블 정의
+        # sqlite 자료형 : INTEGER, REAL, TEXT, BLOB, NULL
+        # https://wikidocs.net/12454, http://lovedb.tistory.com/348 참조
+        self.__dbConnection = sqlite3.connect(self.__dataPath + "\\" + programName + ".db")
+        self.__cursor = self.__dbConnection.cursor()
+        # Account <학번, 이름>
+        self.__cursor.execute('''create table `Account` (
+                                `studentNumber` INTEGER NOT NULL PRIMARY KEY,
+                                `name` TEXT )
+                                ''')
+        # Lecture <강의코드, 교수님성함, 교수님연락처, 교수님메일, 학수번호, 분반, 수강생수, 학점, 성적비중, 한줄메모, 수업날짜, 폴더경로>
+        self.__cursor.execute('''create table `Lecture` (
+                                `lectureCode` TEXT NOT NULL PRIMARY KEY,
+                                `professorName` TEXT,
+                                `professorContact` TEXT,
+                                `professorMail` TEXT,
+                                `academicNumber` TEXT,
+                                `classNumber` INTEGER,
+                                `totalStudentNumber` INTEGER,
+                                `grades` INTEGER,
+                                `scoreRatio` TEXT,
+                                `memo` TEXT,
+                                `dateTime` TEXT,
+                                `directory` TEXT )
+                                ''')
+        # CourseList <강의코드, 학번>
+        self.__cursor.execute('''create table `CourseList` (
+                                `lectureCode` TEXT NOT NULL,
+                                `studentNumber` INTEGER NOT NULL,
+                                PRIMARY KEY (lectureCode,studentNumber) )
+                                ''')
+        # Notice <id, 제목, 작성일, 조회수, 내용, 파일링크>
+        self.__cursor.execute('''create table `Notice` (
+                                `id` INTEGER NOT NULL PRIMARY KEY,
+                                `title` TEXT NOT NULL,
+                                `writeDate` TEXT,
+                                `hitNumber` INTEGER,
+                                `contents` TEXT,
+                                `fileLink` TEXT )
+                                ''')
+        # Assignment <id, 과제이름, 과제내용, 제출시작일, 제출마감일, 연장제출일, 제출완료여부, 제출일자, 평가점수, 강의코드, 학번, 팀과제여부>
+        self.__cursor.execute('''create table `Assignment` (
+                                `id` INTEGER NOT NULL PRIMARY KEY,
+                                `title` TEXT NOT NULL,
+                                `contents` TEXT,
+                                `startDate` TEXT,
+                                `deadLine` TEXT,
+                                `extenedDate` TEXT,
+                                `status` TEXT,
+                                `submitDate` TEXT,
+                                `score` TEXT,
+                                `lectureCode` TEXT,
+                                `studentNumber` INTEGER,
+                                `team` TEXT)
+                                ''')
+        # LearningResource <id, 제목, 작성일, 조회수, 내용, 파일링크>
+        self.__cursor.execute('''create table `LearningResource` (
+                                `id` INTEGER NOT NULL PRIMARY KEY,
+                                `title` TEXT NOT NULL,
+                                `writeDate` TEXT,
+                                `hitNumber` INTEGER,
+                                `contents` TEXT,
+                                `fileLink` TEXT )
+                                ''')
+        # Bookmark <학번, 종류(유형), id>
+        self.__cursor.execute('''create table `BookMark` )
+                                `studentNumber` INTEGER,
+                                `type` TEXT,
+                                `id` INTEGER,
+                                PRIMARY KEY (studentNumber, type, id) ) 
+                                ''')
+        # Exam <강의코드, 시험날짜, 시험시작시간, 시험종료시간, 강의실위치>
+        # 미구현
+        # Cancled <강의코드, 휴강날짜, 휴강시간>
+        # 미구현
+        # Reinforcement <강의코드, 보강날짜, 보강시작시간, 보강종료시간, 강의실>
+        # 미구현
+
+        self.__dbConnection.commit()
+        self.__dbConnection.close()
+
+    def __init(self):
+        if (self.__firstCheck() is True):
+            self.__firstInit()
+        self.__dbConnection = sqlite3.connect(self.__dataPath + "\\" + programName + ".db")
+        self.__cursor = self.__dbConnection.cursor()
+
+    def query(self, query):
+        self.__cursor.execute(query)
+        self.__dbConnection.commit()
+
+class Lecture():
+
+
+user = User.getInstance()
+loginResult = user.login('id', 'password')
+print(loginResult)
