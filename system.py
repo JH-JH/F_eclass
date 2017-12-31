dc7a27a3accacfde0af386aac976699fd053c2c1
노준형 <njsnacker@gmail.com>
2017-12-31 18:14:57 +0900

    학기별 강의정보 스크래핑 완료

--- system.py
+++ system.py
@@ -4,11 +4,13 @@ import requests, rsa, re
 
 programName = "donggukEclassHelper"
 
+
 class User():
     __Instance = None
     __studentNumber = None
     __name = None
     __session = None
+    __campus = "서울캠퍼스"
 
     def __init__(self):
         if self.__Instance is not None:
@@ -22,8 +24,16 @@ class User():
             cls.__Instance = User()
         return cls.__Instance
 
-    def getInfo(self):
-        return {'studentNumber':self.__studentNumber, 'name':self.__name}
+    def getInfo(self,t):
+        if t is "studentNumber":
+            return self.__studentNumber
+        elif t is "campus":
+            return self.__campus
+        elif t is "name":
+            return self.__name
+        else:
+            return "oops!"
+
 
     def login(self, studentNumber, password):
         id = str(studentNumber).encode()
@@ -114,6 +124,12 @@ class Data():
                                 `professorName` TEXT,
                                 `professorContact` TEXT,
                                 `professorMail` TEXT,
+                                `assistant1Name` TEXT,
+                                `assistant1Contact` TEXT,
+                                `assistant1Mail` TEXT,
+                                `assistant2Name` TEXT,
+                                `assistant2Contact` TEXT,
+                                `assistant2Mail` TEXT,
                                 `academicNumber` TEXT,
                                 `classNumber` INTEGER,
                                 `totalStudentNumber` INTEGER,
@@ -200,7 +216,15 @@ class Lecture():
     __professorMail = None
     __professorContact = None
     __totalStudentNumber = None
+    __grades = None
     __dateTime = None
+    __assistant1Name = None
+    __assistant1Contact = None
+    __assistant1Mail = None
+    __assistant2Name = None
+    __assistant2Contact = None
+    __assistant2Mail = None
+
     #사용자 작성
     __scoreRatio = None
     __memo = None
@@ -209,7 +233,7 @@ class Lecture():
 
     def __init__(self,lectureCode, academicNumber, classNumber, professorName, professorMail, profssorContact,
                  totalStudentNumber, dateTime):
-        a = 1
+        NotImplementedError
 
 
     def updateMemo(self):
@@ -234,14 +258,15 @@ class LectureList():
         return cls.__instance
 
     def __init(self):
-        user = User.getInstance()
-        userInfo = user.getInfo()
-        studentNumber = userInfo['studentNumber']
+        #학기 목록 확인
+        #로컬데이터 load
+        #eclass 업데이터 체크
+        #업데이트 확인
 
-
+        #해당 학기의
         self.__getSemesterList()
         for key, value in self.__semesterList.items():
-            courseList = self.__refineLectureData(key)
+            courseList = self.__scrapLectureData(key)
 
 
     #사용자가 이수했던 학기목록을 파악
@@ -269,7 +294,8 @@ class LectureList():
                 self.__semesterList[option.attrs['value']] = option.text
 
     #강의데이터에 들어갈 항목들을 학기별로 정제
-    def __refineLectureData(self,semesterCode):
+    def __scrapLectureData(self,semesterCode):
+        lectureDataList = []
         user = User.getInstance()
         user.sessionGet("https://eclass.dongguk.edu/Main.do?cmd=viewHome")  # 홈으로 초기화
         # 페이지 이동
@@ -288,16 +314,17 @@ class LectureList():
         response = user.sessionPost(url,params)
         soup = BeautifulSoup(response.text,'html.parser')
         soupResult = soup.find_all('table',{'class':'boardListBasic'})
-        lecture = {}
         if soupResult.__len__() is not 0:
             soup = soupResult[0]
-            for lecture in soup.tbody:
-                if lecture != "\n":
+            for item in soup.tbody:
+                lecture = {}
+                lecture.clear()
+                if item != "\n":
                     exp = r"""(?<=<td><a href="javascript:viewStudyHome\(').+(?='\)">)"""
-                    m = re.search(exp, str(lecture))
+                    m = re.search(exp, str(item))
                     lecture['lectureCode'] = m.group()
                     exp = r"""(?<='\)">).+(?=</a></td>)"""
-                    m = re.search(exp, str(lecture))
+                    m = re.search(exp, str(item))
                     lecture['lectureName'] = m.group()
 
                     # https://eclass.dongguk.edu/Course.do?cmd=viewCourseInfo&boardInfoDTO.boardInfoGubun=course_info&courseDTO.courseId=S2017U0002003UDES330701&mainDTO.parentMenuId=menu_00047&mainDTO.menuId=menu_00053
@@ -314,43 +341,92 @@ class LectureList():
                     tempSoup = soupResult[0]
                     exp = r"""(?<=\n\t\t\t\t_).{2}"""
                     m = re.search(exp, str(tempSoup.a.text))
-                    print(m)
+                    lecture['classNumber'] = m.group()
 
                     soupResult = soup.find_all('table',{'summary':'강의실에 관련한 기본정보 리스트입니다.'})
                     tempSoup = soupResult[0]
-                    exp = r"""(?<=</i>과목명\n\t\t\t\t</th>\n<td class="textLeft">\n\t\t\t\t\t).*"""
-                    m = re.search(exp,str(tempSoup))
-                    print(m)
                     exp = r"""(?<=</i>학수번호\n\t\t\t\t</th>\n<td class="textLeft">\n\t\t\t\t\t).*"""
                     m = re.search(exp, str(tempSoup))
-                    print(m)
+                    lecture['academicNumber'] = m.group()
                     exp = r"""(?<=</i>학점\n\t\t\t\t</th>\n<td class="textLeft">\n\t\t\t\t\t).*(?=학점)"""
                     m = re.search(exp, str(tempSoup))
-                    print(m)
+                    lecture['grades'] = m.group()
                     exp = r"""(?<=</i>수강생수\n\t\t\t\t</th>\n<td class="textLeft">\n\t\t\t\t\t).*(?=명)"""
                     m = re.search(exp, str(tempSoup))
-                    print(m)
+                    lecture['totalStudentNumber'] = m.group()
 
                     soupResult = soup.find_all('table', {'summary': '강의실에 관련한 과목운영자 리스트입니다.'})
                     tempSoup = soupResult[0]
-                    """     __professorMail = None
-                            __professorContact = None
-                            __dateTime = None
-                            이렇게 남았음. 근데 조교연락처는 어떻게하지?
-                    """
+                    assistantCount = 0
+                    for tr in tempSoup.find_all('tr'):
+                        tds = tr.find_all('td')
+                        if tds.__len__() is not 0:
 
+                            if (tds[1].text=="책임교수"):
+                                lecture['professorName'] = tds[3].text
+                                lecture['professorMail'] = tds[5].text
+                            elif (tds[1].text=="조교"):
+                                if assistantCount <= 2:
+                                    assistantCount+=1
+                                    lecture['assistant' + str(assistantCount) + 'Name'] = tds[3].text
+                                    lecture['assistant' + str(assistantCount) + 'Mail'] = tds[5].text
 
+                    #https://eclass.dongguk.edu/Course.do?cmd=viewCoursePlanChapterListNew&boardInfoDTO.boardInfoGubun=course_plan&courseDTO.courseId=S2017U0002001UCSE402902&mainDTO.parentMenuId=menu_00047&mainDTO.menuId=menu_00052
+                    url = "https://eclass.dongguk.edu/Course.do"
+                    params = {'cmd': 'viewCoursePlanChapterListNew',
+                              'boardInfoDTO.boardInfoGubun': 'course_plan',
+                              'courseDTO.courseId': lecture['lectureCode'],
+                              'mainDTO.parentMenuId': 'menu_00047',
+                              'mainDTO.menuId': 'menu_00052'}
+                    response = user.sessionPost(url, params)
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    soupResult = soup.find_all('table', {'summary': '강의계획서에 관련한 기본정보 리스트입니다.'})
+                    tempSoup = soupResult[0]
 
+                    exp = r"""(?<=<th class="head">강의실/수업시간</th>\n<td class="textLeft" colspan="3">).*(?=</td>)"""
+                    m = re.search(exp, str(tempSoup))
+                    lecture['dateTime'] = m.group()
+                    exp = r"""(?<=<tr>\n<th class="head">연락처2\(휴대폰\)</th>\n<td class="textLeft">).*(?=</td>)"""
+                    m = re.search(exp, str(tempSoup))
+                    if m is not None :
+                        lecture['professorContact'] = m.group()
+                    else:
+                        lecture['professorContact'] = ""
 
+                    someAssist = {}
+                    exp = r"""(?<=<th class="head">연락처2\(휴대폰\)</th>\n<td class="textLeft">).*(?=</td>\n</tr>)"""
+                    m = re.search(exp,str(soup))
+                    if m is not None :
+                        someAssist['contact'] = m.group()
+                    else:
+                        someAssist['contact'] = ""
 
-
-
-        #print(1)
-
-
+                    exp = r"""(?<=<th class="head">이름</th>\n<td class="textLeft">).*(?=</td>\n</tr>)"""
+                    m = re.search(exp, str(soup))
+                    if m is not None :
+                        someAssist['name'] = m.group()
+                    else:
+                        someAssist['name'] = ""
+                    exp = r"""(?<=<th class="head">e\-메일</th>\n<td class="textLeft">).*(?=</td>\n</tr>)"""
+                    m = re.search(exp, str(soup))
+                    if m is not None:
+                        someAssist['mail'] = m.group()
+                    else:
+                        someAssist['mail'] = ""
+                    for i in range(1,3):
+                        if  'assistant'+str(i)+'Name' in lecture: #조교가 있을때
+                            if ((lecture['assistant'+str(i)+'Name'] == someAssist['name']) or
+                                (lecture['assistant' + str(i) + 'Mail'] == someAssist['mail'])) and not(
+                                    ((lecture['assistant'+str(i)+'Name'] =="")or (lecture['assistant' + str(i) + 'Mail']==""))):
+                                lecture['assistant' + str(i) + 'Contact'] = someAssist['contact']
+                                if (lecture['assistant'+str(i)+'Mail'] != someAssist['mail']):
+                                    lecture['assistant' + str(i) + 'Mail'] += ' / '+someAssist['mail']
+                    lectureDataList.append(lecture)
+        return lectureDataList
 
     def addList(self,lecture):
         NotImplementedError
+
 
 
 class LectureFactory():
@@ -361,7 +437,7 @@ class LectureFactory():
             raise ValueError("instance already exist!")
         else:
             #constructor
-            a = 1
+            NotImplementedError
 
     @classmethod
     def getInstance(cls):
